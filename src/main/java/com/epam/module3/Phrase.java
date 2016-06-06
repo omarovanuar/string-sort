1cbafe2f28e019625f1467e0b8b4bb326ea1ce4b
Anuar Omarov <anuar_omarov@epam.com>
2016-06-06 15:50:01 +0600

    ver 1.2 add Phrase class, fixed all structure)

---
+++ src/main/java/com/epam/module3/Phrase.java
@@ -0,0 +1,29 @@
+package com.epam.module3;
+
+public class Phrase {
+    private static final char[] vowel = {'a', 'e', 'y', 'u', 'i', 'o', 'A', 'E', 'Y', 'U', 'I', 'O'};
+    private String value;
+    private int vowelNumber;
+
+    public Phrase(String value) {
+        this.value = value;
+        vowelNumber = countVowelNumber();
+    }
+
+    private int countVowelNumber() {
+        char[] chars = value.toCharArray();
+        int counter = 0;
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < vowel.length; j++) {
+                if (vowel[j] == chars[i]) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    public Integer getVowelNumber() {
+        return vowelNumber;
+    }
+}
--- src/main/java/com/epam/module3/Runner.java
+++ src/main/java/com/epam/module3/Runner.java
@@ -1,15 +1,35 @@
 package com.epam.module3;
 
+import java.util.*;
+
 public class Runner {
+    private static final Comparator<Phrase> VOWEL_NUMBER_COMPARATOR = (o1, o2) -> o1.getVowelNumber().compareTo(o2.getVowelNumber());
+
     public static void main(String[] args) {
-        StringArray stringArray = new StringArrayFactory().getStringArray();
-        stringArray.countVowelNumber();
+        boolean repeat = true;
+        List<Phrase> list = new ArrayList<>();
+        int number = 0;
+
+        while (repeat) {
+            System.out.println("Please, enter number of strings:");
+            try {
+                number = new Scanner(System.in).nextInt();
+                repeat = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Please, enter integer number");
+            }
+        }
+
+        for (int i = 1; i <= number; i++) {
+            System.out.println("Enter your " + i + " string");
+            list.add(new Phrase(new Scanner(System.in).nextLine()));
+        }
 
         System.out.println("\nArray with number of vowels:");
-        stringArray.showArrays();
+        list.forEach(System.out::println);
 
         System.out.println("\nSorted array by number of vowels:");
-        stringArray.sortByVowelNumber();
-        stringArray.showArrays();
+        Collections.sort(list, VOWEL_NUMBER_COMPARATOR);
+        list.forEach(System.out::println);
     }
 }
